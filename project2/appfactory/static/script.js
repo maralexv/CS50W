691a0e6afad09dca0fa33ebe00990665c59fa76e
[maralexv] <[marchenko.alexander@me.com]>
2019-01-17 16:53:25 +0100

    revamped to OPA

--- project2/appfactory/static/script.js
+++ project2/appfactory/static/script.js
@@ -1,7 +1,21 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-	document
-	}
+// document.addEventListener('DOMContentLoaded', function () {
+// 	document.querySelector('#submit').onclick = function user() {
+// 		const username = document.querySelector('#username').value;
+// 		load_page("channels");
+// 		document.querySelector(".startpage").style.display = 'none';
+// 		return username;
+// 	}
+// });
 
 
-)
+
+
+
+
+
+function load_page(page) {
+	const request = new XMLHttpRequest();
+	request.open('GET', `/${page}`);
+	return false;
+}
